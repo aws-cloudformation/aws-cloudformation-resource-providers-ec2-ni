264f12ce972f51a5e1c00c8942db42af1a011348
Kiran Karpurapu <karpkira@amazon.com>
2020-10-15 19:30:25 -0500

    adding files that got generated as part of cfn init
    cr https://code.amazon.com/reviews/CR-36429805

---
+++ aws-ec2-networkinsightspath/src/main/java/software/amazon/ec2/networkinsightspath/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.ec2.networkinsightspath;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-ec2-networkinsightspath.json");
+    }
+}
