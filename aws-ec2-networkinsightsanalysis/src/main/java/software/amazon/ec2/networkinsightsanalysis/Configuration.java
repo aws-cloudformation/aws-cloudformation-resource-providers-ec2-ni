bf8f0da2be9642ec4375a264c1f3515a7a2007cd
Kiran Karpurapu <karpkira@amazon.com>
2020-10-27 10:59:52 -0500

    defining schema for analysis
    cr https://code.amazon.com/reviews/CR-37284601

---
+++ aws-ec2-networkinsightsanalysis/src/main/java/software/amazon/ec2/networkinsightsanalysis/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.ec2.networkinsightsanalysis;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-ec2-networkinsightsanalysis.json");
+    }
+}
